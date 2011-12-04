55ea5ae013767614c6cf2ae377f82e62a96dd294
drewlesueur <drewalex@gmail.com>
2011-12-04 05:50:25 +0000

    little test

---
+++ a.js
@@ -0,0 +1,30 @@
+//Explicit hard coded paths because I am tired of the other way
+// this way is kind of like how you use it in a browser
+
+require("./public/dmodule.js")
+require("./public/all-func.js")
+express = require("express");
+dModule.define("express", function () {return express;});
+require("./public/express-rpc.js")
+require("./public/severus2.js")
+var twilio = require("twilio")
+dModule.define("twilio", function() { return twilio })
+require("./public/mobilemin-app.js")
+require("./public/mobilemin-twilio.js")
+require("./public/mobilemin-server.js")
+var _ = require("./public/underscore.js")
+dModule.define("underscore", function () { return _ });
+var nimble = require("./public/nimble.js")
+dModule.define("nimble", function () { return nimble });
+require("./public/drews-mixins.js")
+require("./config.js")
+
+var config = dModule.require("config")
+var _ = dModule.require("underscore")
+
+
+Server = dModule.require("mobilemin-server")
+server = Server("init")()
+server("handleNewCustomerWhoTextedStart")({},"+1488405406" )
+
+
